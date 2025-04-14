4651f4caf3efffb7d8abb315dbd61d8d55b25b95
Mitchell Torok <35317575+Mitchell-Torok@users.noreply.github.com>
2025-04-15 00:09:08 +1000

    Delete install directory

--- install/interfaces/share/interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaces_IDL_FILES "msg/MotionCaptureState.idl;msg/ELRSCommand.idl")
-set(interfaces_INTERFACE_FILES "msg/MotionCaptureState.msg;msg/ELRSCommand.msg")
