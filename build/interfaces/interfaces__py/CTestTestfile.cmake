a65c6aba1f617e875ddc02530e49cf355b9b0137
Mitchell Torok <35317575+Mitchell-Torok@users.noreply.github.com>
2025-04-15 00:09:17 +1000

    Delete build directory

--- build/interfaces/interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mitchell/Documents/PhD/drone_cage_control/build/interfaces/interfaces__py
-# Build directory: /home/mitchell/Documents/PhD/drone_cage_control/build/interfaces/interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/motion_capture_publisher/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mitchell/Documents/PhD/drone_cage_control/install/motion_capture_publisher'
