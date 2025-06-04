5e7924829096befc0b5b8d636245e2b816e3f751
Mitchell <mitch.torok@gmail.com>
2025-06-04 10:56:23 +1000

    Added RL controller base

---
+++ src/controller_RL/controller_RL/main.py
@@ -0,0 +1,102 @@
+import rclpy
+import signal
+import sys
+import numpy as np
+from rclpy.node import Node
+from .gui import GUI
+from interfaces.msg import MotionCaptureState, ELRSCommand
+from geometry_msgs.msg import Pose, PoseArray
+
+
+class Controller(Node):
+    def __init__(self):
+        super().__init__('controller')
+        self.cmd_publisher_ = self.create_publisher(ELRSCommand, '/ELRSCommand', 10)
+        self.pose_subscription_ = self.create_subscription(MotionCaptureState, '/motion_capture_state', self.pose_callback, 10)
+
+        self.current_pose = None
+        self.setpoint = np.array([0.0, 0.0, 1.0])
+
+        # Set up control loop
+        self.control_frequency = 30.0
+        self.dt = 1.0 / self.control_frequency
+        self.timer = self.create_timer(self.dt, self.control_loop)
+
+        self.pre_start_counter = 0
+        self.pre_start_steps = self.control_frequency
+        self.armed = False
+
+        self.gui = GUI(self)
+
+
+    # Recieve motion capture data
+    def pose_callback(self, msg: MotionCaptureState):
+        position = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
+        orientation =  np.array([msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z])
+        linear_velocity = np.array([msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z])
+        angular_velocity = np.array([msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z])
+        self.current_pose = np.concatenate((position, orientation, linear_velocity, angular_velocity))
+
+    def control_loop(self):
+
+        # For saftey generate a message with all channels set to 0.0
+        msg = ELRSCommand()
+        msg.armed = False
+        msg.channel_0 = 0.0
+        msg.channel_1 = 0.0
+        msg.channel_2 = 0.0
+        msg.channel_3 = 0.0
+
+        # Pre-start state: Send 0.05 on all channels for one second before starting control loop.
+        if self.armed and self.pre_start_counter < self.pre_start_steps:
+            msg.armed = True
+            msg.channel_0 = 0.05
+            msg.channel_1 = 0.05
+            msg.channel_2 = 0.05
+            msg.channel_3 = 0.05
+            self.pre_start_counter += 1
+
+        elif self.armed and self.current_pose is not None:
+            state = self.current_pose
+            goal = self.setpoint
+
+
+            # CONTROL CODE GOES HERE
+
+
+
+            msg.armed = True
+            msg.channel_0 = 0.1
+            msg.channel_1 = 0.1
+            msg.channel_2 = 0.1
+            msg.channel_3 = 0.1
+        else:         
+            self.pre_start_counter = 0
+
+        self.cmd_publisher_.publish(msg)
+        print(f"control output {msg.channel_0}, {msg.channel_1}, {msg.channel_2}, {msg.channel_3}")
+
+    # GUI functions
+    def signal_handler(self, sig, frame):
+        self.on_close()
+
+    def on_close(self):
+        self.gui.quit()
+        rclpy.shutdown()
+        sys.exit(0)
+
+
+
+
+def main(args=None): 
+    rclpy.init(args=args)
+    controller = Controller()
+    signal.signal(signal.SIGINT, controller.signal_handler)
+    while rclpy.ok():
+        rclpy.spin_once(controller, timeout_sec=0.1)
+        controller.gui.handle_events()
+    controller.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
